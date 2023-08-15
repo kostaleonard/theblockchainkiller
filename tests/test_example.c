1f9e72f7472deab95d791d640b966423b6b4bfba
leonard.kosta <kostaleonard@gmail.com>
2023-08-15 14:20:34 -0400

    Added valgrind and test memory leak for CI
    Signed-off-by: leonard.kosta <kostaleonard@gmail.com>

--- tests/test_example.c
+++ tests/test_example.c
@@ -1,8 +1,14 @@
 #include "example.h"
 #include "test_example.h"
+//TODO remove leak
+#include <stdio.h>
+#include <stdlib.h>
 
 void test_square_returns_perfect_square()
 {
+    //TODO remove leak
+    void *p = malloc(50);
+    printf("%p\n", p);
     assert_true(9 == square(3));
     assert_true(25 == square(5));
 }
