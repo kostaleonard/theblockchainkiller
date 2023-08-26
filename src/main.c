24e02ca8d2f8ebd44e13767dcec72865a9f0f8fc
leonard.kosta <kostaleonard@gmail.com>
2023-08-26 08:30:38 -0400

    Added main function
    Signed-off-by: leonard.kosta <kostaleonard@gmail.com>

--- src/main.c
+++ src/main.c
@@ -3,8 +3,28 @@
  */
 
 #include <stdio.h>
+#include "include/blockchain.h"
+#include "include/block.h"
 
 int main(int argc, char **argv) {
-    printf("Hello world!\n");
-    return 0;
+    return_code_t return_code = SUCCESS;
+    blockchain_t *blockchain = NULL;
+    block_t *genesis_block = NULL;
+    return_code = blockchain_create(&blockchain);
+    if (SUCCESS != return_code) {
+        goto end;
+    }
+    return_code = block_create_genesis_block(&genesis_block);
+    if (SUCCESS != return_code) {
+        goto end;
+    }
+    return_code = blockchain_add_block(blockchain, genesis_block);
+    if (SUCCESS != return_code) {
+        block_destroy(genesis_block);
+        goto end;
+    }
+    blockchain_print(blockchain);
+end:
+    blockchain_destroy(blockchain);
+    return return_code;
 }
