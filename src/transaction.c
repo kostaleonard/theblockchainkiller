0e1ed30bc13366099758c142e7fc2c86329f3f85
leonard.kosta <kostaleonard@gmail.com>
2023-10-16 09:34:41 -0400

    Added transaction create and destroy
    Signed-off-by: leonard.kosta <kostaleonard@gmail.com>

--- src/transaction.c
+++ src/transaction.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "include/transaction.h"
 
 return_code_t transaction_create(
@@ -6,9 +7,32 @@ return_code_t transaction_create(
     uint32_t recipient_id,
     uint32_t amount
 ) {
-    return FAILURE_INVALID_INPUT;
+    return_code_t return_code = SUCCESS;
+    if (NULL == transaction) {
+        return_code = FAILURE_INVALID_INPUT;
+        goto end;
+    }
+    transaction_t *new_transaction = malloc(sizeof(transaction_t));
+    if (NULL == new_transaction) {
+        return_code = FAILURE_COULD_NOT_MALLOC;
+        goto end;
+    }
+    new_transaction->created_at = time(NULL);
+    new_transaction->sender_id = sender_id;
+    new_transaction->recipient_id = recipient_id;
+    new_transaction->amount = amount;
+    *transaction = new_transaction;
+end:
+    return return_code;
 }
 
 return_code_t transaction_destroy(transaction_t *transaction) {
-    return FAILURE_INVALID_INPUT;
+    return_code_t return_code = SUCCESS;
+    if (NULL == transaction) {
+        return_code = FAILURE_INVALID_INPUT;
+        goto end;
+    }
+    free(transaction);
+end:
+    return return_code;
 }
