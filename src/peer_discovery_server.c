4dd0a76571578094cc7ea333bd0da91570163b70
leonard.kosta <kostaleonard@gmail.com>
2024-10-14 09:16:39 -0400

    Added peer discovery server hello world
    Signed-off-by: leonard.kosta <kostaleonard@gmail.com>

---
+++ src/peer_discovery_server.c
@@ -0,0 +1,12 @@
+/**
+ * @brief Runs the peer discovery server.
+ */
+
+#include <stdio.h>
+#include "include/return_codes.h"
+
+int main(int argc, char **argv) {
+    return_code_t return_code = SUCCESS;
+    printf("Hello peer discovery server\n");
+    return return_code;
+}
