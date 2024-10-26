c55ad41062550003cafcb5f076870009b7223c14
leonard.kosta <kostaleonard@gmail.com>
2024-10-26 03:51:44 -0400

    Initial refactor of peer discovery server template
    Signed-off-by: leonard.kosta <kostaleonard@gmail.com>

--- src/peer_discovery_server.c
+++ src/peer_discovery_server.c
@@ -2,6 +2,7 @@
  * @brief Runs the peer discovery server.
  */
 
+#include <stdbool.h>
 #include <stdio.h>
 //#include <unistd.h> // TODO is this available on Windows in general?
 // TODO the only reason I support cross-platform builds is to be able to test locally without using a container. Is there a less messy way to do that?
@@ -17,6 +18,8 @@
 #endif
 #include "include/return_codes.h"
 
+#define LISTEN_BACKLOG 5
+
 int main(int argc, char **argv) {
     return_code_t return_code = SUCCESS;
     printf("Hello peer discovery server\n");
@@ -25,87 +28,77 @@ int main(int argc, char **argv) {
     // TODO just before sending the current peer list, filter out any expired peer addresses.
 
     // TODO clean this code up
+    if (2 != argc) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        return_code = FAILURE_INVALID_COMMAND_LINE_ARGS;
+        goto end;
+    }
+    int port = strtol(argv[1], NULL, 10);
 #ifdef _WIN32
-    WSADATA wsaData; //TODO unused variable?
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        printf("WSAStartup failed.\n");
-        return 1;
-    }
-#endif
-    int listenfd; /* listening socket */
-    int connfd; /* connection socket */
-    int portno; /* port to listen on */
-    int clientlen; /* byte size of client's address */
-    struct sockaddr_in serveraddr = {0}; /* server's addr */
-    struct sockaddr_in clientaddr = {0}; /* client addr */
-    struct hostent *hostp = {0}; /* client host info */
-    char buf[BUFSIZ]; /* message buffer */
-    char *hostaddrp; /* dotted decimal host addr string */
-    int optval; /* flag value for setsockopt */
-    int n; /* message byte size */
-
-    /* check command line args */
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s <port>\n", argv[0]);
-        exit(1);
-    }
-    portno = atoi(argv[1]);
-
-    /* socket: create a socket */
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (listenfd < 0) {
-        printf("Failed to create socket\n");
+    WSADATA wsaData;
+    if (0 != WSAStartup(MAKEWORD(2, 2), &wsaData)) {
         return_code = FAILURE_NETWORK_FUNCTION;
         goto end;
     }
+#endif
+    int clientlen; /* byte size of client's address */
+    struct sockaddr_in server_addr = {0};
+    struct sockaddr_in client_addr = {0};
+    // TODO gethostbyname is deprecated: https://man7.org/linux/man-pages/man3/gethostbyaddr.3.html
+    struct hostent *hostp = {0}; /* client host info */
+    char buf[BUFSIZ];
+    char *hostaddrp; /* dotted decimal host addr string */
+    int n; /* message byte size */
 
-    /* setsockopt: Handy debugging trick that lets 
-    * us rerun the server immediately after we kill it; 
-    * otherwise we have to wait about 20 secs. 
-    * Eliminates "ERROR on binding: Address already in use" error. 
-    */
-    optval = 1;
-    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, 
-            (const void *)&optval , sizeof(int));
+    
+    
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd < 0) {
+        return_code = FAILURE_NETWORK_FUNCTION;
+        goto end;
+    }
+    int optval = 1;
+    if (0 != setsockopt(
+        listen_fd,
+        SOL_SOCKET,
+        SO_REUSEADDR,
+        (const void *)&optval,
+        sizeof(int))) {
+        return_code = FAILURE_NETWORK_FUNCTION;
+        goto end;
+    }
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons((unsigned short)port);
 
-    serveraddr.sin_family = AF_INET; /* we are using the Internet */
-    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); /* accept reqs to any IP addr */
-    serveraddr.sin_port = htons((unsigned short)portno); /* port to listen on */
-
-    /* bind: associate the listening socket with a port */
-    if (bind(listenfd, (struct sockaddr *) &serveraddr, 
-        sizeof(serveraddr)) < 0) {
+    if (bind(listen_fd, (struct sockaddr *) &server_addr, 
+        sizeof(server_addr)) < 0) {
             return_code = FAILURE_NETWORK_FUNCTION;
             goto end;
         }
 
-    /* listen: make it a listening socket ready to accept connection requests */
-    /* allow 5 requests to queue up */ 
-    if (listen(listenfd, 5) < 0) {
+    if (listen(listen_fd, LISTEN_BACKLOG) < 0) {
         return_code = FAILURE_NETWORK_FUNCTION;
         goto end;
     }
 
-    /* main loop: wait for a connection request, echo input line, 
-        then close connection. */
-    clientlen = sizeof(clientaddr);
-    while (1) {
-
-        /* accept: wait for a connection request */
-        connfd = accept(listenfd, (struct sockaddr *) &clientaddr, &clientlen);
+    clientlen = sizeof(client_addr);
+    while (true) {
+        // TODO may need to multi-thread this loop? But probably for our small application, using the main thread is fine.
+        int connfd = accept(listen_fd, (struct sockaddr *) &client_addr, &clientlen);
         if (connfd < 0) {
             return_code = FAILURE_NETWORK_FUNCTION;
             goto end;
         }
         
-        /* gethostbyaddr: determine who sent the message */
-        hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
-                sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+        // TODO gethostbyaddr is deprecated
+        hostp = gethostbyaddr((const char *)&client_addr.sin_addr.s_addr, 
+                sizeof(client_addr.sin_addr.s_addr), AF_INET);
         if (hostp == NULL) {
             return_code = FAILURE_NETWORK_FUNCTION;
             goto end;
         }
-        hostaddrp = inet_ntoa(clientaddr.sin_addr);
+        hostaddrp = inet_ntoa(client_addr.sin_addr);
         if (hostaddrp == NULL) {
             return_code = FAILURE_NETWORK_FUNCTION;
             goto end;
@@ -121,14 +114,13 @@ int main(int argc, char **argv) {
         }
         printf("server received %d bytes: %s", n, buf);
         
-        /* write: echo the input string back to the client */
         n = send(connfd, buf, strlen(buf), 0); // TODO not sure the return code can be negative
         if (n < 0) {
             return_code = FAILURE_NETWORK_FUNCTION;
             goto end;
         }
 
-        // TODO conditional compile
+        // TODO conditional compile (closesocket is Windows)
         //close(connfd);
         closesocket(connfd);
     }
