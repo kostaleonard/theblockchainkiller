6bb97bfa2198751635bcb1bb2e661f7b183f1157
leonard.kosta <kostaleonard@gmail.com>
2024-10-26 05:03:00 -0400

    Completed peer discovery server refactor
    Signed-off-by: leonard.kosta <kostaleonard@gmail.com>

--- src/peer_discovery_server.c
+++ src/peer_discovery_server.c
@@ -28,30 +28,21 @@ int main(int argc, char **argv) {
     // TODO accepts connections, adds IP info to peer list, sends current peer list to peer that just connected, closes connection with peer
     // TODO IP information in peer list expires after 60 seconds, and peers are responsible for reconnecting with the server every now and then to refresh.
     // TODO just before sending the current peer list, filter out any expired peer addresses.
-
-    // TODO clean this code up
     if (2 != argc) {
         fprintf(stderr, "Usage: %s <port>\n", argv[0]);
         return_code = FAILURE_INVALID_COMMAND_LINE_ARGS;
         goto end;
     }
     int port = strtol(argv[1], NULL, 10);
-#ifdef _WIN32
-    WSADATA wsaData;
-    if (0 != WSAStartup(MAKEWORD(2, 2), &wsaData)) {
-        return_code = FAILURE_NETWORK_FUNCTION;
-        goto end;
-    }
-#endif
-    int clientlen; /* byte size of client's address */
+    #ifdef _WIN32
+        WSADATA wsaData;
+        if (0 != WSAStartup(MAKEWORD(2, 2), &wsaData)) {
+            return_code = FAILURE_NETWORK_FUNCTION;
+            goto end;
+        }
+    #endif
     struct sockaddr_in server_addr = {0};
     struct sockaddr_in client_addr = {0};
-    // TODO gethostbyname is deprecated: https://man7.org/linux/man-pages/man3/gethostbyaddr.3.html
-    struct hostent *hostp = {0}; /* client host info */
-    char *hostaddrp; /* dotted decimal host addr string */
-
-    
-    
     int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (listen_fd < 0) {
         return_code = FAILURE_NETWORK_FUNCTION;
@@ -70,63 +61,64 @@ int main(int argc, char **argv) {
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons((unsigned short)port);
-
-    if (bind(listen_fd, (struct sockaddr *) &server_addr, 
-        sizeof(server_addr)) < 0) {
-            return_code = FAILURE_NETWORK_FUNCTION;
-            goto end;
-        }
-
-    if (listen(listen_fd, LISTEN_BACKLOG) < 0) {
+    if (bind(
+        listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         return_code = FAILURE_NETWORK_FUNCTION;
         goto end;
     }
-
-    clientlen = sizeof(client_addr);
+    if (listen(listen_fd, LISTEN_BACKLOG) < 0) {
+        return_code = FAILURE_NETWORK_FUNCTION;
+        goto end;
+    }    
     while (true) {
         // TODO may need to multi-thread this loop? But probably for our small application, using the main thread is fine.
-        int connfd = accept(listen_fd, (struct sockaddr *) &client_addr, &clientlen);
-        if (connfd < 0) {
+        int client_len = sizeof(client_addr);
+        int conn_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &client_len);
+        if (conn_fd < 0) {
             return_code = FAILURE_NETWORK_FUNCTION;
             goto end;
         }
-        
-        // TODO gethostbyaddr is deprecated
-        hostp = gethostbyaddr((const char *)&client_addr.sin_addr.s_addr, 
-                sizeof(client_addr.sin_addr.s_addr), AF_INET);
-        if (hostp == NULL) {
+        if (sizeof(client_addr) != client_len) {
+            return_code = FAILURE_BUFFER_TOO_SMALL;
+            goto end;
+        }
+        char client_hostname[NI_MAXHOST] = {0};
+        if (0 != getnameinfo((struct sockaddr *)&client_addr, client_len, client_hostname, NI_MAXHOST, NULL, 0, 0)) {
             return_code = FAILURE_NETWORK_FUNCTION;
             goto end;
         }
-        hostaddrp = inet_ntoa(client_addr.sin_addr);
-        if (hostaddrp == NULL) {
+        char client_addr_str[INET_ADDRSTRLEN] = {0};
+        if (NULL == inet_ntop(AF_INET, &client_addr.sin_addr, client_addr_str, INET_ADDRSTRLEN)) {
             return_code = FAILURE_NETWORK_FUNCTION;
             goto end;
         }
-        printf("server established connection with %s (%s)\n", hostp->h_name, hostaddrp);
-        
+        printf("Server established connection with %s (%s)\n", client_hostname, client_addr_str);
         char buf[BUFSIZ] = {0};
-        int message_len = recv(connfd, buf, BUFSIZ, 0); // TODO not sure the return code can be negative
+        // TODO handle partial read/write
+        int message_len = recv(conn_fd, buf, BUFSIZ, 0); // TODO not sure the return code can be negative
         if (message_len < 0) {
             return_code = FAILURE_NETWORK_FUNCTION;
             goto end;
         }
-        printf("server received %d bytes: %s", message_len, buf);
-        
-        message_len = send(connfd, buf, strlen(buf), 0); // TODO not sure the return code can be negative
+        printf("Server received %d bytes: %s", message_len, buf);
+        // TODO handle partial read/write
+        message_len = send(conn_fd, buf, strlen(buf), 0); // TODO not sure the return code can be negative
         if (message_len < 0) {
             return_code = FAILURE_NETWORK_FUNCTION;
             goto end;
         }
         # ifdef _WIN32
-            closesocket(connfd);
+            closesocket(conn_fd);
         # else
-            close(connfd);
+            close(conn_fd);
         # endif
     }
+    #ifdef _WIN32
+        closesocket(listen_fd);
+        WSACleanup();
+    # else
+        close(listen_fd);
+    #endif
 end:
-#ifdef _WIN32
-    WSACleanup();
-#endif
     return return_code;
 }
