248d257093b749fab25479d11d0f98540e94d2af
leonard.kosta <kostaleonard@gmail.com>
2024-11-28 10:42:37 -0500

    Added peer list deserialization
    Signed-off-by: leonard.kosta <kostaleonard@gmail.com>

--- src/peer_discovery.c
+++ src/peer_discovery.c
@@ -2,6 +2,7 @@
  * @brief Contains functions for peer discovery.
  */
 
+#include <stdio.h>
 #include "include/peer_discovery.h"
 #include "include/endian.h"
 
@@ -89,7 +90,92 @@ return_code_t peer_info_list_deserialize(
         return_code = FAILURE_INVALID_INPUT;
         goto end;
     }
-    // TODO
+    unsigned char *next_spot_in_buffer = buffer;
+    ptrdiff_t total_read_size = next_spot_in_buffer + sizeof(uint64_t) - buffer;
+    if (total_read_size > buffer_size) {
+        return_code = FAILURE_BUFFER_TOO_SMALL;
+        goto end;
+    }
+    uint64_t num_peers = betoh64(*(uint64_t *)next_spot_in_buffer);
+    next_spot_in_buffer += sizeof(num_peers);
+    linked_list_t *new_peer_info_list = NULL;
+    return_code = linked_list_create(
+        &new_peer_info_list, free, compare_peer_info_t);
+    if (SUCCESS != return_code) {
+        goto end;
+    }
+    for (uint64_t peer_idx = 0; peer_idx < num_peers; peer_idx++) {
+        total_read_size = next_spot_in_buffer + sizeof(uint16_t) - buffer;
+        if (total_read_size > buffer_size) {
+            return_code = FAILURE_BUFFER_TOO_SMALL;
+            linked_list_destroy(new_peer_info_list);
+            goto end;
+        }
+        uint16_t sin6_family = ntohs(*(uint16_t *)next_spot_in_buffer);
+        next_spot_in_buffer += sizeof(sin6_family);
+        total_read_size = next_spot_in_buffer + sizeof(uint16_t) - buffer;
+        if (total_read_size > buffer_size) {
+            return_code = FAILURE_BUFFER_TOO_SMALL;
+            linked_list_destroy(new_peer_info_list);
+            goto end;
+        }
+        uint16_t sin6_port = ntohs(*(uint16_t *)next_spot_in_buffer);
+        next_spot_in_buffer += sizeof(sin6_port);
+        total_read_size = next_spot_in_buffer + sizeof(uint32_t) - buffer;
+        if (total_read_size > buffer_size) {
+            return_code = FAILURE_BUFFER_TOO_SMALL;
+            linked_list_destroy(new_peer_info_list);
+            goto end;
+        }
+        uint32_t sin6_flowinfo = ntohl(*(uint32_t *)next_spot_in_buffer);
+        next_spot_in_buffer += sizeof(sin6_flowinfo);
+        total_read_size = next_spot_in_buffer + sizeof(IN6_ADDR) - buffer;
+        if (total_read_size > buffer_size) {
+            return_code = FAILURE_BUFFER_TOO_SMALL;
+            linked_list_destroy(new_peer_info_list);
+            goto end;
+        }
+        struct in6_addr addr = {0};
+        for (size_t idx = 0; idx < sizeof(IN6_ADDR); idx++) {
+            ((unsigned char *)&addr)[idx] = *next_spot_in_buffer;
+            next_spot_in_buffer++;
+        }
+        total_read_size = next_spot_in_buffer + sizeof(uint32_t) - buffer;
+        if (total_read_size > buffer_size) {
+            return_code = FAILURE_BUFFER_TOO_SMALL;
+            linked_list_destroy(new_peer_info_list);
+            goto end;
+        }
+        uint32_t sin6_scope_id = ntohl(*(uint32_t *)next_spot_in_buffer);
+        next_spot_in_buffer += sizeof(sin6_scope_id);
+        total_read_size = next_spot_in_buffer + sizeof(uint64_t) - buffer;
+        if (total_read_size > buffer_size) {
+            return_code = FAILURE_BUFFER_TOO_SMALL;
+            linked_list_destroy(new_peer_info_list);
+            goto end;
+        }
+        uint64_t last_connected = betoh64(*(uint64_t *)next_spot_in_buffer);
+        next_spot_in_buffer += sizeof(last_connected);
+        peer_info_t *peer = malloc(sizeof(peer_info_t));
+        if (NULL == peer) {
+            return_code = FAILURE_COULD_NOT_MALLOC;
+            linked_list_destroy(new_peer_info_list);
+            goto end;
+        }
+        peer->listen_addr.sin6_family = sin6_family;
+        peer->listen_addr.sin6_port = sin6_port;
+        peer->listen_addr.sin6_flowinfo = sin6_flowinfo;
+        memcpy(&peer->listen_addr.sin6_addr, &addr, sizeof(IN6_ADDR));
+        peer->listen_addr.sin6_scope_id = sin6_scope_id;
+        peer->last_connected = last_connected;
+        return_code = linked_list_append(new_peer_info_list, peer);
+        if (SUCCESS != return_code) {
+            free(peer);
+            linked_list_destroy(new_peer_info_list);
+            goto end;
+        }
+    }
+    *peer_info_list = new_peer_info_list;
 end:
     return return_code;
 }
